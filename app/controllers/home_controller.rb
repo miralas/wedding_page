e9fb0be792daad17597c3cda4d935018749eb886
Miralas <miralas777@yandex.ru>
2017-08-21 09:23:58 +0300

    Fix condition

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    request.ip == '93.100.212.51' ? @aliens = false : @aliens = true
+    request.remote_ip == '93.100.212.51' ? @aliens = false : @aliens = true
     @days = (DateTime.parse("07.10.2017 15:00:00 ").mjd - DateTime.now.mjd).to_i
   end
 
